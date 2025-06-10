f795a1f3badbcd686e5aef27ee843d9c0bb9b755
Danielnorbert22 <Daniel4cod1ng@gmail.com>
2025-06-10 22:03:20 +0100

    update the database for hosting

--- listen.js
+++ listen.js
@@ -1,9 +1,6 @@
-const request = require("supertest");
-const seed = require("./db/seeding/seed.js");
-const data = require("./db/data/test-data/index.js");
-const db = require("./db/connection.js");
 const app = require("./app");
 
-app.listen(9090, () => {
-  console.log("Server is listening on port 9090...");
-});
+const app = require("./app.js");
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
